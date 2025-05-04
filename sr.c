5dba77f9e4c0c790e11e66693092ccd38844d272
Bowen Liu <bowen.liu@student.adelaide.edu.au>
2025-05-05 04:33:24 +0930

    123231

--- sr.c
+++ sr.c
@@ -71,18 +71,11 @@ void A_output(struct msg message)
   int seqfirst = seq_a;
   int seqlast = (seq_a + WINDOWSIZE-1) % SEQSPACE;
   int index;
-  /* Check if A_nextseqnum is within the sender window */
-  bool in_window = false;
-  if (seqfirst <= seqlast) {
-    if (A_nextseqnum >= seqfirst && A_nextseqnum <= seqlast)
-      in_window = true;
-  } else {
-    if (A_nextseqnum >= seqfirst || A_nextseqnum <= seqlast)
-      in_window = true;
-  }
 
   /* if not blocked waiting on ACK */
-  if (in_window) {
+  if (((seqfirst <= seqlast) && (A_nextseqnum >= seqfirst && A_nextseqnum <= seqlast)) ||
+      ((seqfirst > seqlast) && (A_nextseqnum >= seqfirst || A_nextseqnum <= seqlast)))
+  {
     if (TRACE > 1)
       printf("----A: New message arrives, send window is not full, send new messge to layer3!\n");
 
